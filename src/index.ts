0789397638b230a6e6681ecd415a4b28de5c2097
Alita Moore <alita.moore805@gmail.com>
2021-03-16 01:22:05 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ require('module-alias/register')
 import { main } from "./main";
 import { __MAIN__ } from "./utils";
 
+console.log("this is master")
 const isDebug = process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
 isDebug ? __MAIN__() : main();
