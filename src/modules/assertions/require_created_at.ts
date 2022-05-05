ddd1fa94332f249e7d8a29f945a504b9595f51ad
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-05 01:37:21 -0400

    Update require_created_at.ts

--- src/modules/assertions/require_created_at.ts
+++ src/modules/assertions/require_created_at.ts
@@ -1,6 +1,7 @@
 // Issue #65, Originally from #55
 // Immediately before merging (IMO, ideally in the merge commit itself) set the created date in the preamble.
 // Creator: SamWilsn
+// Alternative solution: warn author to include created_at date
 
 // to be included when merged in /src/modules/assertions 
 
@@ -11,11 +12,10 @@ import {
   PR,
   Review
 } from "src/domain";
-import _ from "lodash"; 
 	
 export const getCreatedAt = () => { 
 	const github = getOctokit(GITHUB_TOKEN).rest;
-	 console.log(`Warning: Please ensure to include in template PR_Created_date: `+context.payload?.pull_request?.created_at);
+	 console.log(`Warning: Please ensure to include in the preamble PR_Created_date: `+context.payload?.pull_request?.created_at);
 
 }; 
 
