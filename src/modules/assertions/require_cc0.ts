1bf7ad5042ea570e4e69197ee3106ccb1bd927d6
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-04 22:01:57 -0400

    Add files via upload
    require_cc0.ts to be added to /src/modules/assertions

---
+++ src/modules/assertions/require_cc0.ts
@@ -0,0 +1,34 @@
+// Issue #70, Originally from #55
+// Require CC0 waiver 
+// Creator: SamWilsn
+
+// to be included when merged in /src/modules/assertions
+
+import { context, getOctokit } from "@actions/github";
+import { setFailed } from "@actions/core";
+import {
+  GITHUB_TOKEN,
+  GithubSelf,
+  PR,
+  Review
+} from "src/domain";
+import _ from "lodash";
+
+// BOT will fail if no CC0 detected in PR body
+// Just after: Verify correct environment and request context in main.ts, if no CC0 is detected, 
+// BOT will fail and comment: "error: No CC0 waiver detected" 
+
+export const  checkCC0 = () => {
+        
+	const github = getOctokit(GITHUB_TOKEN).rest;
+	if !(github.pulls.get(context.repo.body).search("CC0")) {
+		console.log(`Critical error: No CC0 waiver detected`);
+		System.exit(1);
+	}	
+
+};
+
+// Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
+
+
+
