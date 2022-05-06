bf8d95a0385d22fa5e82450efc6b6d70d876aa43
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-06 18:52:29 -0400

    Update src/modules/assertions/require_cc0.ts
    Co-authored-by: Micah Zoltu <micah@zoltu.net>

--- src/modules/assertions/require_cc0.ts
+++ src/modules/assertions/require_cc0.ts
@@ -16,9 +16,9 @@ export const  checkCC0 = () => {
         
 	const github = getOctokit(GITHUB_TOKEN).rest;
   const copyrightRegex = /^[\s\S]*## Copyright\n*Copyright and related rights waived via \[CC0\]\(.+\)\.\n*$/g;
-	if (context.payload?.pull_request?.body && copyrightRegex.test(context.payload?.pull_request?.body)) {
-		console.log(`Critical error: CC0 Copyright must be the last thing in the EIP.`);
-		System.exit(1);
-	}	
+  if (context.payload?.pull_request?.body && copyrightRegex.test(context.payload?.pull_request?.body)) {
+    console.log(`Critical error: CC0 Copyright must be the last thing in the EIP.`);
+    System.exit(1);
+  }
 
 };
