5a5f4f8f02d00a491a4ba1c28e6e539cad27dd31
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-06 22:36:41 -0400

    Update require_cc0.ts

--- src/modules/assertions/require_cc0.ts
+++ src/modules/assertions/require_cc0.ts
@@ -15,9 +15,21 @@ import _ from "lodash";
 export const  checkCC0 = () => {
         
 	const github = getOctokit(GITHUB_TOKEN).rest;
-  const copyrightRegex = /^[\s\S]*## Copyright\n*Copyright and related rights waived via \[CC0\]\(.+\)\.\n*$/g;
-  if (context.payload?.pull_request?.body && copyrightRegex.test(context.payload?.pull_request?.body)) {
-    console.log(`Critical error: CC0 Copyright must be the last thing in the EIP.`);
+  // const copyrightRegex = /^[\s\S]*## Copyright\n*Copyright and related rights waived via \[CC0\]\(.+\)\.\n*$/g;
+  // if (context.payload?.pull_request?.body && copyrightRegex.test(context.payload?.pull_request?.body)) {
+  //  console.log(`Critical error: CC0 Copyright must be the last thing in the EIP.`);
+    
+  const expected = `\n## Copyright\nCopyright and related rights waived via [CC0](../LICENSE.md).`
+  const body = context.payload?.pull_request?.body
+  
+  // TODO: we should figure out what conditions allow us to reach this code with `body` being null.
+  //could BOT force PUSH license into empty body (not empty anymore, is it allowed ?) ? Question..
+  
+  if (body === null) return;
+  if (body.endsWith(expected) || body.endsWith(`${expected}\n`) return;
+  console.log(`Critical error: CC0 Copyright must be the last thing in the EIP.  The file should end with:\n## Copyright\nCopyright and related rights waived via [CC0](../LICENSE.md).`);
+  System.exit(1);  
+    
     System.exit(1);
   }
 
