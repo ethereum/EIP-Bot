b1c50193d55997c4f4267a9b7b60246702158fc6
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-08 00:24:43 -0400

    Update require_created_at.ts

--- src/modules/assertions/require_created_at.ts
+++ src/modules/assertions/require_created_at.ts
@@ -8,7 +8,7 @@ import {
 	
 export const getCreatedAt = () => { 
   const github = getOctokit(GITHUB_TOKEN).rest;
-  const createRegex = /^[\s\S]*created:  \d\d\d\d-\d\d-\d\d\ n?/g;
+  const createRegex = /^[\s\S]*created:  \d\d\d\d-\d\d-\d\d\n?/g;
   if !(createRegex.test(context.payload?.pull_request?.body)) {
     console.warn(`Warning: Please ensure to include in the preamble created date: ${context.payload?.pull_request?.created_at}`);
   }
