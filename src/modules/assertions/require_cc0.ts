41c160e6efece8543c2757ee615a33e82cda44f9
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-13 13:58:47 -0700

    Update require_cc0.ts

--- src/modules/assertions/require_cc0.ts
+++ src/modules/assertions/require_cc0.ts
@@ -13,9 +13,6 @@ export const  requireCC0 = () => {
 	const github = getOctokit(GITHUB_TOKEN).rest;
   const copyrightRegex = /^[\s\S]*\n## Copyright\nCopyright and related rights waived via \[CC0\]\(\.\.\/LICENSE.md\)\.\n?$/g
   
-  // TODO: we should figure out what conditions allow us to reach this code with `body` being null.
-  // If body is null there is no license, EIP-BOT must exit(1). let me know if you agree with it..
-  
   if !(context.payload?.pull_request?.body) return;
   if (context.payload?.pull_request?.body && !(copyrightRegex.test(context.payload?.pull_request?.body))) {
     console.log(`Critical error: CC0 Copyright must be the last thing in the EIP.  The file should end with:\n## Copyright\nCopyright and related rights waived via [CC0](../LICENSE.md).`);
