1c3f9c99d6792738d43d593839a51bbbc2d490e5
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-23 12:36:15 -0700

    Update require_max_file_number.ts

--- src/modules/assertions/require_max_file_number.ts
+++ src/modules/assertions/require_max_file_number.ts
@@ -4,11 +4,11 @@ import {
 } from "#/assertions";
 import { github } from "src/infra/github";
 
-export const  requireMaxFileNumber = () => {
+export const  requireMaxFileNumber = async (): Promise <>  {
   const max_files_allowed = 100;
   requireEvent();
-  const pr = requirePr();
-  const files = github.getPullRequestFiles(pr.number);
+  const pr = await requirePr();
+  const files = await github.getPullRequestFiles(pr.number);
   if ((files?.length) > max_files_allowed) {
     console.log(`Critical error: Number of PR Files > ${max_files_allowed}`);
     process.exit(1);
