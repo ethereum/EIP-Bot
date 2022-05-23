708a64984f674ad0b0028f23b89440d0ab79f45f
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-23 13:58:58 -0700

    Update require_inline_coding.ts

--- src/modules/assertions/require_inline_coding.ts
+++ src/modules/assertions/require_inline_coding.ts
@@ -1,8 +1,9 @@
+import { context } from "@actions/github";
 import _ from "lodash";
 
 export const  requireInlineCoding = () => {
   const sol_file_Regex = /http:\/\/\S+.sol/g;
-  if (sol_file_Regex.test(context.payload?.pull_request?.body) {
+  if (sol_file_Regex.test(context.payload?.pull_request?.body)) {
     console.log(`EIP-BOT is terminating. Please replace external link http://.../file.sol for inline code reference..`);
     process.exit(1);
   }
