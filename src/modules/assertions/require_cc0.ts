505778fbd96f295d3823fa5d63737a77a4ac8f93
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-23 00:29:45 -0700

    Update require_cc0.ts

--- src/modules/assertions/require_cc0.ts
+++ src/modules/assertions/require_cc0.ts
@@ -5,9 +5,8 @@ import {
 import _ from "lodash";
 
 export const  requireCC0 = () => {
-  const github = getOctokit(GITHUB_TOKEN).rest;
   const copyrightRegex = /^[\s\S]*\n## Copyright\n{1,2}Copyright and related rights waived via \[CC0\]\(\.\.\/LICENSE.md\)\.\n?$/g;
-  if !(context.payload?.pull_request?.body) { return 0; } 
+  if (!(context.payload?.pull_request?.body)) { return 0; } 
   if (context.payload?.pull_request?.body && !(copyrightRegex.test(context.payload?.pull_request?.body))) {
     console.log(`Critical error: CC0 Copyright must be the last thing in the EIP.  The file should end with:\n## Copyright\nCopyright and related rights waived via [CC0](../LICENSE.md).`);
     process.exit(1);
