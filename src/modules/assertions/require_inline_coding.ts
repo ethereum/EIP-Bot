2be7eecf5448c6a4a5267282549658bafcd7978f
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-26 22:50:03 -0700

    Update require_inline_coding.ts

--- src/modules/assertions/require_inline_coding.ts
+++ src/modules/assertions/require_inline_coding.ts
@@ -1,19 +1,19 @@
 import { context } from "@actions/github";
 import _ from "lodash";
 export const  requireInlineCoding = () => {
-  
-  //const solfileRegex = /http:\/\/\S+.sol/g;
+  //test
+  const solfileRegex = /http:\/\/\S+.sol/g;
   const prBodyString = (context.payload?.pull_request?.body != null) ? JSON.stringify(context.payload?.pull_request?.body) : [];
-  if (prBodyString != null) {
-    /*const prBodyRegexMatches = (prBodyString.match(solfileRegex) !=null) ? prBodyString.match(solfileRegex) : []; 
-    if (!(prBodyString) || (prBodyString.length === 0)) {return 0;}
-    if (prBodyRegexMatches != null) {
+  const prBodyRegexMatches = (prBodyString != null) ? prBodyString.match(solfileRegex) : []; 
+  
+  if (!(prBodyString) || (prBodyString.length === 0)) {return 0;}
+  /*  if (prBodyRegexMatches != null) {
       for (let i = 0; i < prBodyRegexMatches.length; i++) {
         console.log(`EIP-BOT is terminating. Please replace external link http://.../file.sol: ${prBodyRegexMatches[i]} for inline code reference..`);
       }*/
       //process.exit(1);
-    return 0;
+  //  return 0;
     //} 
-  }
+  //}
   return 1;
 };
