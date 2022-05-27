691e18854d8c3c69b2091cff7b1a1cb649fd549f
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-26 22:26:28 -0700

    Update require_inline_coding.ts

--- src/modules/assertions/require_inline_coding.ts
+++ src/modules/assertions/require_inline_coding.ts
@@ -1,17 +1,18 @@
 import { context } from "@actions/github";
 import _ from "lodash";
 export const  requireInlineCoding = () => {
+  // test
   const solfileRegex = /http:\/\/\S+.sol/g;
-  if (context.payload?.pull_request?.body != null) {
-   const prBodyString= JSON.stringify(context.payload?.pull_request?.body);
-   const prBodyRegexMatches = prBodyString.match(solfileRegex) !=null ? prBodyString.match(solfileRegex) : []; 
-   if (!(prBodyString) || (prBodyString.length === 0)) {return 0;}
-   if (prBodyRegexMatches != null) {
-    for (let i = 0; i < prBodyRegexMatches.length; i++) {
-      console.log(`EIP-BOT is terminating. Please replace external link http://.../file.sol: ${prBodyRegexMatches[i]} for inline code reference..`);
-    }
-    process.exit(1);
-   } 
+  const prBodyString = (JSON.stringify(context.payload?.pull_request?.body) != null) ? JSON.stringify(context.payload?.pull_request?.body) : [];
+  if prBodyString != null {
+    const prBodyRegexMatches = (prBodyString.match(solfileRegex) !=null) ? prBodyString.match(solfileRegex) : []; 
+    if (!(prBodyString) || (prBodyString.length === 0)) {return 0;}
+    if (prBodyRegexMatches != null) {
+      for (let i = 0; i < prBodyRegexMatches.length; i++) {
+        console.log(`EIP-BOT is terminating. Please replace external link http://.../file.sol: ${prBodyRegexMatches[i]} for inline code reference..`);
+      }
+      process.exit(1);
+    } 
   }
   return 1;
 };
