0c69d39cbc57abf31664b386487b8105c8d16fa3
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-26 18:05:18 -0700

    Update require_inline_coding.ts

--- src/modules/assertions/require_inline_coding.ts
+++ src/modules/assertions/require_inline_coding.ts
@@ -4,7 +4,7 @@ import _ from "lodash";
 export const  requireInlineCoding = () => {
   const solfileRegex = /http:\/\/\S+.sol/g;
   const prBodyString= JSON.stringify(context.payload?.pull_request?.body);
-  const prBodyRegexMatches = prBodyString.match(solfileRegex)!=null ? prBodyString.match(solfileRegex) , [];
+  const prBodyRegexMatches = prBodyString.match(solfileRegex)!=null ? prBodyString.match(solfileRegex) : [];
     
   if (!(prBodyString) || (prBodyString.length === 0)) {return 0;}
   if (prBodyRegexMatches.length > 0) {
