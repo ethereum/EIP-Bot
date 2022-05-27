ab462a0bc166705920950447000f6f53fa6707f6
Jose Alfonso <92005826+JEAlfonsoP@users.noreply.github.com>
2022-05-26 17:25:41 -0700

    Update require_inline_coding.ts

--- src/modules/assertions/require_inline_coding.ts
+++ src/modules/assertions/require_inline_coding.ts
@@ -2,10 +2,12 @@ import { context } from "@actions/github";
 import _ from "lodash";
 
 export const  requireInlineCoding = () => {
+  const prBodyRegexMatches? : string[];
   const solfileRegex = /http:\/\/\S+.sol/g;
   const prBodyString = JSON.stringify(context.payload?.pull_request?.body);
-  const prBodyRegexMatches? = prBodyString.match(solfileRegex);
+
   if (!(prBodyString) || (prBodyString.length === 0)) {return 0;}
+  prBodyRegexMatches = prBodyString.match(solfileRegex);
   if ((prBodyRegexMatches) || (prBodyRegexMatches.length > 0)) {
     for (let i = 0; i < prBodyRegexMatches.length; i++) {
       console.log(`EIP-BOT is terminating. Please replace external link http://.../file.sol: ${prBodyRegexMatches[i]} for inline code reference..`);
