2af064abb1a575cb7ffc73290c122841b732420e
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2022-08-10 15:58:28 -0400

    Migrate to NodeJS Action (#123)
    * Use NodeJS action
    
    * Get inputs directly from github
    
    * Oops

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 require("module-alias/register");
+import * as core from "@actions/core";
 import { main } from "./main";
 import { NodeEnvs } from "src/domain";
 import { __MAIN__ } from "#/utils/debug";
 import { __MAIN_MOCK__ } from "src/tests/assets/mockPR";
+
+const defaultEnv = {
+  ERC_EDITORS: core.getInput("ERC_EDITORS"),
+  CORE_EDITORS: core.getInput("CORE_EDITORS"),
+  NETWORKING_EDITORS: core.getInput("NETWORKING_EDITORS"),
+  INTERFACE_EDITORS: core.getInput("INTERFACE_EDITORS"),
+  META_EDITORS: core.getInput("META_EDITORS"),
+  INFORMATIONAL_EDITORS: core.getInput("INFORMATIONAL_EDITORS"),
+  GITHUB_TOKEN: core.getInput("GITHUB-TOKEN"),
+  MAINTAINERS: core.getInput("MAINTAINERS"),
+  PR_NUMBER: core.getInput("PR_NUMBER"),
+  NODE_ENV: "production"
+};
+
+for (let property in defaultEnv) {
+  if (!process.env[property]) {
+    process.env[property] = defaultEnv[property];
+  }
+}
 
 const isDebug =
   process.env.NODE_ENV === NodeEnvs.developemnt ||
